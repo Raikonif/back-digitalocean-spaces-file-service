7669c4a8a2739a13ce5e0a4dade91bd1e6ebf222
Raikonif <raikonif@gmail.com>
2024-06-13 20:56:36 -0400

    initial service code for digitalocean spaces

---
+++ main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI, HTTPException, Query
+from fastapi.responses import JSONResponse
+import boto3
+from botocore.exceptions import NoCredentialsError, PartialCredentialsError
+import os
+from dotenv import load_dotenv
+
+app = FastAPI()
+
+load_dotenv()
+s3_client = boto3.client(
+    's3',
+    region_name='nyc3',  # Reemplaza con tu región
+    endpoint_url=os.getenv("DIGITAL_OCEAN_ORIGIN"),  # Reemplaza con tu endpoint
+    aws_access_key_id=os.getenv("DIGITAL_OCEAN_ACCESS_KEY"),
+    aws_secret_access_key=os.getenv("DIGITAL_OCEAN_SECRET_KEY")
+)
+
+
+@app.get("/generate-presigned-url")
+async def generate_presigned_url(bucket_name: str = Query(...), key: str = Query(...)):
+    try:
+        url = s3_client.generate_presigned_url(
+            'put_object',
+            Params={
+                'Bucket': bucket_name,
+                'Key': key,
+                'ACL': 'public-read'
+            },
+            ExpiresIn=60
+        )
+        return JSONResponse(content={"url": url})
+    except NoCredentialsError:
+        raise HTTPException(status_code=500, detail="Credenciales no encontradas")
+    except PartialCredentialsError:
+        raise HTTPException(status_code=500, detail="Credenciales incompletas")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
